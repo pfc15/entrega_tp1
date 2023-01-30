3f1532b291adb334ed8b8c9eb14a87874e18f1de
pfc15 <52254091+pfc15@users.noreply.github.com>
2023-01-29 23:30:08 -0300

    Add files via upload

---
+++ Lista_musica/scr/modelo/Mae.java
@@ -0,0 +1,45 @@
+package modelo;
+import java.util.*;
+
+public class Mae extends Critico {
+	
+	private List<String> apelidos_carinhosos = new ArrayList<String>();
+	
+	public Mae(Musica analise) {
+		nome = "mãe";
+		apelidos_carinhosos.add("carinho");
+		apelidos_carinhosos.add("amor");
+		frases_positivas.add("muito interessante");
+		frases_positivas.add("acho que ouvi no radio hoje de manhã");
+		this.musica_analisada = analise;
+		this.veredito = apuração();
+	}
+	
+	public Mae(Musica analise, List<String> positivo, List<String> apelidos) {
+		nome = "mãe";
+		this.apelidos_carinhosos = apelidos;
+		Random rand = new Random();
+		for (int i=0; i<positivo.size();i++) {
+			int num = rand.nextInt(apelidos_carinhosos.size());
+			frases_positivas.add("ah " +apelidos_carinhosos.get(num) + " " + positivo.get(i));
+		}
+		this.musica_analisada = analise;
+		
+		this.veredito = apuração();
+		
+	}
+	
+	@Override
+	public boolean apuração() {
+		return true;
+	}
+	
+	public List<String> getApelidos_carinhosos(){
+		return apelidos_carinhosos;
+	}
+	
+	public void setApelidos_carinhosos(List<String> apelidos_carinhosos) {
+		this.apelidos_carinhosos = apelidos_carinhosos;
+	}
+	
+}
---
+++ Lista_musica/scr/modelo/Pedante.java
@@ -0,0 +1,43 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedante extends Critico{
+	
+	private List<String> apelidos_degradantes = new ArrayList<String>();
+	
+	public Pedante(Musica musica_analisda) {
+		apelidos_degradantes.add("ignorante");
+		apelidos_degradantes.add("seu alienado");
+		frases_negativas.add("a harmonia é tão previsível");
+		frases_negativas.add("espero não ouvir isso de novo");
+		this.musica_analisada = musica_analisda;
+		this.veredito = apuração();
+	}
+	
+	public Pedante(Musica música_analisada, List<String> negativo, List<String> apelidos) {
+		this.nome = "pedante";
+		this.apelidos_degradantes = apelidos;
+		this.frases_negativas = negativo;
+		this.musica_analisada = música_analisada;
+		this.veredito = apuração();
+		
+	}
+	
+	
+	public List<String> getApelidos_degradantes() {
+		return apelidos_degradantes;
+	}
+	public void setApelidos_degradantes(List<String> apelidos_degradantes) {
+		this.apelidos_degradantes = apelidos_degradantes;
+	}
+	
+	@Override
+	public boolean apuração() {
+		return false;
+	}
+	
+	
+	
+}
